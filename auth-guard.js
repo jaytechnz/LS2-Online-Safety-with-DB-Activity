3e5ab0ff8018f009c1abfb192d260458bf0510ab
Jenny Smith <53411671+jaytechnz@users.noreply.github.com>
2026-06-09 15:47:48 +1200

    Add files via upload

--- auth-guard.js
+++ auth-guard.js
@@ -1,3 +1,15 @@
 import { requireRole } from "./auth-utils.js";
 
-requireRole(["student", "teacher", "superadmin"]);
+function goToLogin() {
+  const page = window.location.pathname.split("/").pop() || "index.html";
+  window.location.href = `login.html?next=${encodeURIComponent(page)}`;
+}
+
+const fallbackTimer = setTimeout(goToLogin, 5000);
+
+requireRole(["student", "teacher", "superadmin"])
+  .then(() => clearTimeout(fallbackTimer))
+  .catch(() => {
+    clearTimeout(fallbackTimer);
+    goToLogin();
+  });
